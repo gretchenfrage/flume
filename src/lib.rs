ca949b110791b49793a3eedb80c9bed655d718e7
Phoenix Kahlo <kahlo.phoenix@gmail.com>
2024-10-17 23:27:11 -0500

    move iterators into own module

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,12 @@ pub mod r#async;
 
 mod signal;
 mod error;
+mod iterator;
 
 // Reexports
 pub use select::Selector;
 pub use self::error::*;
+pub use self::iterator::*;
 
 use std::{
     collections::VecDeque,
@@ -735,107 +737,6 @@ impl<T> Drop for Receiver<T> {
         if self.shared.receiver_count.fetch_sub(1, Ordering::Relaxed) == 1 {
             self.shared.disconnect_all();
         }
-    }
-}
-
-/// This exists as a shorthand for [`Receiver::iter`].
-impl<'a, T> IntoIterator for &'a Receiver<T> {
-    type Item = T;
-    type IntoIter = Iter<'a, T>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        Iter { receiver: self }
-    }
-}
-
-impl<T> IntoIterator for Receiver<T> {
-    type Item = T;
-    type IntoIter = IntoIter<T>;
-
-    /// Creates a self-owned but semantically equivalent alternative to [`Receiver::iter`].
-    fn into_iter(self) -> Self::IntoIter {
-        IntoIter { receiver: self }
-    }
-}
-
-/// An iterator over the msgs received from a channel.
-pub struct Iter<'a, T> {
-    receiver: &'a Receiver<T>,
-}
-
-impl<'a, T> fmt::Debug for Iter<'a, T> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Iter").field("receiver", &self.receiver).finish()
-    }
-}
-
-impl<'a, T> Iterator for Iter<'a, T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.receiver.recv().ok()
-    }
-}
-
-/// An non-blocking iterator over the msgs received from a channel.
-pub struct TryIter<'a, T> {
-    receiver: &'a Receiver<T>,
-}
-
-impl<'a, T> fmt::Debug for TryIter<'a, T> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.debug_struct("TryIter").field("receiver", &self.receiver).finish()
-    }
-}
-
-impl<'a, T> Iterator for TryIter<'a, T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.receiver.try_recv().ok()
-    }
-}
-
-/// An fixed-sized iterator over the msgs drained from a channel.
-#[derive(Debug)]
-pub struct Drain<'a, T> {
-    queue: VecDeque<T>,
-    /// A phantom field used to constrain the lifetime of this iterator. We do this because the
-    /// implementation may change and we don't want to unintentionally constrain it. Removing this
-    /// lifetime later is a possibility.
-    _phantom: PhantomData<&'a ()>,
-}
-
-impl<'a, T> Iterator for Drain<'a, T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.queue.pop_front()
-    }
-}
-
-impl<'a, T> ExactSizeIterator for Drain<'a, T> {
-    fn len(&self) -> usize {
-        self.queue.len()
-    }
-}
-
-/// An owned iterator over the msgs received from a channel.
-pub struct IntoIter<T> {
-    receiver: Receiver<T>,
-}
-
-impl<T> fmt::Debug for IntoIter<T> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.debug_struct("IntoIter").field("receiver", &self.receiver).finish()
-    }
-}
-
-impl<T> Iterator for IntoIter<T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.receiver.recv().ok()
     }
 }
 
